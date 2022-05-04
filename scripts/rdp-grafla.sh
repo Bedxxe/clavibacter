e0aee16de7a86b3b4b9c10733632be9a0f0acf75
Diego Garfias Gallegos <67386612+Bedxxe@users.noreply.github.com>
2022-05-03 20:31:25 -0500

    Add files via upload

---
+++ scripts/rdp-grafla.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+mkdir grap-files
+
+export2graphlan.py --skip_rows 1,2 -i mpa-files/combine.mpa --tree grap-files/merged_abundance.tree.txt \
+--annotation grap-files/merged_abundance.annot.txt --most_abundant 80 --annotations 2 \
+--external_annotations 6 --abundance_threshold 15 \
+--annotation_legend_font_size 8 --def_font_size 30
+echo Output files saved inside grap-files folder
+
+echo Color for Proteobacteria changed from $#2d19ff to $#e6ab02
+sed 's/#2d19ff/#7570b3/g' grap-files/merged_abundance.annot.txt > temp.txt && mv temp.txt grap-files/merged_abundance.annot.txt
+
+echo Color for Actinobacteria changed from $#29cc36 to $#e7298a
+sed 's/#29cc36/#1b9e77/g' grap-files/merged_abundance.annot.txt > temp.txt && mv temp.txt grap-files/merged_abundance.annot.txt
+
+echo Color for Firmicutes changed from $#ff3333 to $#d95f03
+sed 's/#ff3333/#d95f03/g' grap-files/merged_abundance.annot.txt > temp.txt && mv temp.txt grap-files/merged_abundance.annot.txt
+
+echo Color for Chloroflexi changed from $#00bfff to $#1b9e77
+sed 's/#00bfff/#e7298a/g' grap-files/merged_abundance.annot.txt > temp.txt && mv temp.txt grap-files/merged_abundance.annot.txt
+
+echo Color for Bacteroidetes changed from $#00ff80 to $#7570b3
+sed 's/#00ff80/#e6ab02/g' grap-files/merged_abundance.annot.txt > temp.txt && mv temp.txt grap-files/merged_abundance.annot.txt
+
+
+graphlan_annotate.py --annot grap-files/merged_abundance.annot.txt grap-files/merged_abundance.tree.txt grap-files/merged_abundance.xml
+
+echo Generating the .png file
+graphlan.py --dpi 300 --size 10 grap-files/merged_abundance.xml rpd-graphlan_graph.png --external_legends
