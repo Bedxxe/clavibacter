76889f365820c10ad6a28a4cc82846cee5008056
Diego Garfias Gallegos <67386612+Bedxxe@users.noreply.github.com>
2022-07-04 14:41:52 -0500

    Update kraken-reads.sh

--- scripts/kraken-reads.sh
+++ scripts/kraken-reads.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # This is a program that is going to pick a SraRunTable of metadata and 
-#extract the run label to run the next programs.
+#extract the run label to download, trim and move the libraries information.
 
-# This program requires that you give 2 input data. 1) where this 
-#SraRunTable is located, and 2) where the kraken database has been saved.
+# This program requires that you give 2 input data: 1) where this 
+#SraRunTable is located, and 2) where the kraken database has been saved
 
+#ASSIGNATIONS
 metd=$1 #Location to the SraRunTable.txt
 kdat=$2 #Location of the kraken2 database
+
 root=$(pwd) #Gets the path to the directory of this file, on which the outputs ought to be created 
 # Now we will define were the reads are:
 runs='reads'
@@ -16,31 +18,27 @@ mkdir reads
 mkdir -p taxonomy/kraken
 mkdir -p taxonomy/taxonomy-logs/scripts
 mkdir -p taxonomy/kraken/reports
-mkdri -p taxonomy/biom-files
+mkdir -p taxonomy/kraken/krakens
 
 # DOWNLOADING THE DATA
 
 #Let's use the next piece of code to download the data
 cat $metd  |  sed -n '1!p' | while read line;  do read=$(echo $line | cut -d',' -f1); fasterq-dump -S $read -p -e 8 -o $read ; done
 mv *.fastq reads/
+# The -e flag can be customized. This indicates the number of threads used to do this task.
 
-# MANAGING THE DATA
+# MANAGING THE DOWNLADED DATA
 
 # We will change the names of the reads files. They have a sufix that makes impossible
 #to be read in a loop
 ls $runs | while read line ; do new=$(echo $line | sed 's/_/-/g'); mv $runs/$line $runs/$new; done
 
 # Now, we will create a file where the information of the run labes can be located
-cat $metd  | while read line; do read=$(echo $line | cut -d',' -f1); echo $read ; done > run-labels.txt
+cat $metd  | sed -n '1!p' | while read line; do read=$(echo $line | cut -d',' -f1); echo $read ; done > run-labels.txt
 mv run-labels.txt metadata/
 
 # TAXONOMIC ASSIGNATION WITH KRAKEN2
 
-cat metadata/run-labels.txt | while read line; do mkdir taxonomy/kraken/$line; file1=$(echo $runs/$line-1.fastq); file2=$(echo $runs/$line-2.fastq) ; echo '\n''working in run' "$line"\ 
-#kraken2 --db $kdat --threads 12 --paired $file1 $file2 --output taxonomy/kraken/$line/$line.kraken --report taxonomy/kraken/$line/$line.report \ 
-echo '#!/bin/sh''\n''\n'"kraken2 --db $kdat --threads 12 --paired" "$runs/$line"'-1.fastq' "$runs/$line"'-2.fastq' "--output taxonomy/kraken/$line/$line.kraken --report taxonomy/kraken/$line/$line.report" > taxonomy/taxonomy-logs/scripts/$line-kraken.sh; sh taxonomy/taxonomy-logs/scripts/$line-kraken.sh; cp taxonomy/kraken/$line/$line.report taxonomy/kraken/reports;done
-
-
-
-
-
+cat metadata/run-labels.txt | while read line; do file1=$(echo $runs/$line-1.fastq); file2=$(echo $runs/$line-2.fastq) ; echo '\n''working in run' "$line"\ 
+#kraken2 --db $kdat --threads 6 --paired $file1 $file2 --output taxonomy/kraken/krakens/$line.kraken --report taxonomy/kraken/reports/$line.report \ 
+echo '#!/bin/sh''\n''\n'"kraken2 --db $kdat --threads 6 --paired" "$runs/$line"'-1.fastq' "$runs/$line"'-2.fastq' "--output taxonomy/kraken/krakens/$line.kraken --report taxonomy/kraken/reports/$line.report" > taxonomy/taxonomy-logs/scripts/$line-kraken.sh; sh taxonomy/taxonomy-logs/scripts/$line-kraken.sh; done
