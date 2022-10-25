21c71d800f5bea14d05d5c6cc450b745eaecb250
shadayguerrero <shadayguerrero@gmail.com>
2022-10-24 23:00:12 -0500

    nota

---
+++ scripts-tda/.ipynb_checkpoints/02-extract-reads-for-SY-checkpoint.sh
@@ -0,0 +1,9 @@
+file=$1
+#This script will run over all SY to extraxct all reads from *sam file's
+base2=$(basename ${file} .txt)
+for infile in output-tda/3TP/sorted/*.sam
+do 
+	base=$(basename ${infile} .sam)
+	head -4 ${infile} > output-tda/3TP/sam_extract/${base2}-${base}.bam  #get the head the .sam file's
+	samtools view ${infile} | grep -f ${file} >> output-tda/3TP/sam_extract/${base2}-${base}.bam # append the read selected 
+	done
